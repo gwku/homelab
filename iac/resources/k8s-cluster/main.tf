8aa6512377bfca4370ab8eb0c03f54e0a266d3df
Gerwin Kuijntjes <administratie@gerwinkuijntjes.nl>
2025-06-28 17:04:08 +0200

    Extracted tofu providers

--- iac/resources/k8s-cluster/main.tf
+++ iac/resources/k8s-cluster/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.2-rc01"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url          = var.pm_api_url
-  pm_api_token_id     = var.pm_api_token_id
-  pm_api_token_secret = var.pm_api_token_secret
-  pm_tls_insecure     = var.pm_tls_insecure
-}
-
 locals {
   # Create a flattened list of VMs with global index for VM ID assignment
   vms_flat = flatten([
